a19c8270b307e0abbc15a214adc28632831c2553
rogerbutt <andrew.luu.93@gmail.com>
2014-09-19 22:34:17 -0500

    Record changes

--- app/scripts/controllers/record.js
+++ app/scripts/controllers/record.js
@@ -7,4 +7,32 @@ angular.module('banterApp')
       'AngularJS',
       'Karma'
     ];
+
+    if (!('webkitSpeechRecognition' in window)) {
+      upgrade();
+    } else {
+      var recognition = new webkitSpeechRecognition();
+      recognition.continuous = true;
+      recognition.interimResults = true;
+
+      recognition.onstart = function() {
+
+      }
+      recognition.onresult = function(event) {
+
+      }
+      recognition.onerror = function(event) {
+
+      }
+      recognition.onend = function() {
+
+      }
+    }
+
+    $('#record').click(function() {
+      final_transcript = '';
+      recognition.lang = 'en-US';
+      recognition.start();
+    });
+
   });
