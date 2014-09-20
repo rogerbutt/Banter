cf1ae4f2aa1994d180f7430092c126cff341b494
lezzago <ashish81394@gmail.com>
2014-09-20 14:36:40 -0500

    more work on similarity
    Got a score for similarity

--- app/scripts/controllers/similarity.js
+++ app/scripts/controllers/similarity.js
@@ -1,4 +1,16 @@
-function similarity(speechText, slideText)
+function similarity(speechText, slideJson, currSlide)
 {
-	
+	var slides = slideJson.slides;
+	var slideText = slides[currSlide].content;
+	slideText.replace("\<.*?\>", "");
+
+	var speechLength = speechText.length;
+	var slideTextLength = slideText.length;
+	var diffLength = slideTextLength - speechLength;
+	if(diffLength < 0)
+		diffLength = 0;
+
+	var leven = new Levenshtein(slideText, speechText);
+	var score = (1 - ((leven - diffLength)/speechLength);
+	return score;
 }
