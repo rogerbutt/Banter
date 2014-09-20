89aced8e124c07359d66314d4b6cb5ab43a911a2
rogerbutt <andrew.luu.93@gmail.com>
2014-09-19 22:18:00 -0500

    Fixed routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,5 +3,20 @@
 angular.module('banterApp', [
   'ngCookies',
   'ngResource',
-  'ngSanitize'
-]);
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/record', {
+        templateUrl: 'views/record.html',
+        controller: 'RecordCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
