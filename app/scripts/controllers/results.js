da8a3756eb11da9299a516a3b1501a2b96bbbe26
rogerbutt <andrew.luu.93@gmail.com>
2014-09-20 17:40:04 -0500

    C3 changes

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -14,20 +14,33 @@ angular.module('banterApp')
 
     var sync = $firebase(ref).$asObject();
     // download the data into a local object
+
+    var axis1 = ['slides'];
+    var axis2 = ['score'];
     sync.$bindTo($scope, "presentation").then(function() {
       if(!$scope.$$phase) {
         $scope.$apply();
       }
-    });
-      var chart = c3.generate({
-      bindto:"#chart",
-      data: {
-        x:"x",
-        columns:[
-        ['x',30,20,100,50,150],
-        ['Similarity',50,10,60,30,60]
-        ]
+
+      for(var i = 0; i < $scope.presentation.slides.length; i++) {
+        axis1.push(i);
+        if($scope.presentation.slides[i].results)
+          axis2.push($scope.presentation.slides[i].results[0]);
+        else
+          axis2.push(0);
       }
-    })
+      
+      var chart = c3.generate({
+        bindto:"#chart",
+        data: {
+          x:"slides",
+          columns:[
+            axis1,
+            axis2
+          ]
+        }
+      });
+    });
+
 
   }]);
